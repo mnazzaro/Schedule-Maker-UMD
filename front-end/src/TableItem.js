c2c3004124a1e824e4d429953b02cd80652ff24f
Mark Nazzaro <marknazzaro2@gmail.com>
2021-03-15 21:28:05 -0400

    nothing is showing up ;)

--- front-end/src/TableItem.js
+++ front-end/src/TableItem.js
@@ -1,7 +1,25 @@
-function TableItem (props) {
-    return (
-        <td><input width="100%" height="100%"/></td>
-    )
+import React from 'React'
+
+class TableItem extends React.Component {
+
+    constructor (props) {
+        this.state = {active: false}
+    }
+
+    render () {
+        return (
+            <td>
+                <div className="dropdown">
+                    <input onkeyup={this.state.active=true} height="100%"/>
+                    <div className="dropdown-content {this.state.active ? show : ''">
+                        <div className="dropdown-item">Option 1</div>
+                        <div className="dropdown-item">Option 2</div>
+                        <div className="dropdown-item">Option 3</div>
+                    </div>
+                </div>
+            </td>
+        )
+    }
 }
 
 export default TableItem;
